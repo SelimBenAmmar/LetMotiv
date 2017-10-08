a96ce350567186dca5bfe2337557cce7893f4be3
Blabla <hackacka@localhost.localdomain>
2017-10-08 15:44:28 +0100

    start new chabot

--- Pascal/data/static_data.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  cities: [
-    { name: 'San Francisco', code: 'SF', image: `${process.env.BOT_BASE_URL}/Djerba.jpg` }
-  ]
-}
--- Pascal/index.js
+++ Pascal/index.js
@@ -3,7 +3,7 @@
 
   This is the programmatic entry point of your bot.
   Your bot's logic resides here.
-
+  
   Here's the next steps for you:
   1. Read this file to understand how this simple bot works
   2. Read the `content.yml` file to understand how messages are sent
@@ -20,62 +20,16 @@
 */
 
 module.exports = function(bp) {
-
-bp.middlewares.load()
-
-bp.hear(/stop|arrêt/i, (event, next) => {
- const convo = bp.convo.find(event)
- convo && convo.stop('conversation arrêtée')
-})
-
-// -------------- Welcome message with 2 quick replies responses: Salut, Tu vas bien ?  ----------------
-bp.hear(/GET_STARTED|salut|cc|coucou|bonjour|bonsoir|hey|hi|hello|hola/i, (event, next) => {
-
-  const txt = txt => bp.messenger.createText(event.user.id, txt)
-
-  bp.convo.start(event, convo => {
-      convo.threads['default'].addMessage('#welcome', [
-        {
-          pattern: 'WELCOME.B1',
-          callback: () => {
-            convo.say(txt('Sympa ;)'))
-            convo.switchTo('bye')
-          }
-        },
-        {
-          pattern: 'WELCOME.B2',
-          callback: () => {
-            convo.say(txt('Ohhh...'))
-            convo.switchTo('bye')
-          }
-        }
-      ])
-
-      convo.createThread('bye')
-      convo.threads['bye'].addMessage('#goodbye')
-
-
-    })
+  // Listens for a first message (this is a Regex)
+  // GET_STARTED is the first message you get on Facebook Messenger
+  bp.hear(/GET_STARTED|hello|hi|test|hey|holla/i, (event, next) => {
+    event.reply('#welcome') // See the file `content.yml` to see the block
   })
-}
-
-/*
-  event.reply('#welcome') // See the file `content.yml` to see the block
-
-})
-bp.hear('WELCOME.B1', (event, next) => {
-  event.reply('#welome_reply', { welcome_res: 'Sympa ;)' })
-})
-bp.hear('WELCOME.B2', (event, next) => {
-  event.reply('#welcome_reply', { welcome_res: 'Ohhh... J espère qu il n y a eu rien de grave' })
-})
-
-
 
   // You can also pass a matcher object to better filter events
   bp.hear({
     type: /message|text/i,
-    text: /exit|bye|goodbye|quit|done|leave|stop|bislema/i
+    text: /exit|bye|goodbye|quit|done|leave|stop/i
   }, (event, next) => {
     event.reply('#goodbye', {
       // You can pass data to the UMM bloc!
@@ -83,4 +37,3 @@ bp.hear('WELCOME.B2', (event, next) => {
     })
   })
 }
-*/
