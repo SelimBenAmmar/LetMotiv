54c37da73c16a28655ab33519d18ea6e498d34e2
SelimBenAmmar <selim.benammar@telecom-paristech.fr>
2017-10-29 14:06:12 +0100

    Connect with sqlite db

--- Pascal/index.js
+++ Pascal/index.js
@@ -1,4 +1,15 @@
 module.exports = function(bp) {
+
+  /* user exists (select) if yes then update else insert
+  const updateUserSkill = (user_id, skillName, skillValue) => {
+    bp.db.get()
+    .then(knex => 
+      knex('skills')
+      .insert({ user_id, skillName: skillValue}));
+  };
+  */
+
+  
   /* Welcome message with :
   (1) quick replies about feeling good or bad
   (2) quick replies about student's role
@@ -18,8 +29,11 @@ module.exports = function(bp) {
 
   // -----------------------------------------------------------------------
   // How many questions we will ask to the student
-  bp.hear('QR_NB_OF_QUESTIONS+' | 'QR_NB_OF_QUESTIONS++', (event, next) => {
-    event.reply('Q_LikeTheActivity', { activity_name: "l'Orient Express" });
+  bp.hear('QR_NB_OF_QUESTIONS+', (event, next) => {
+    event.reply('#Q_LikeTheActivity', { activity_name: "l'Orient Express" });
+  });
+  bp.hear('QR_NB_OF_QUESTIONS++', (event, next) => {
+    event.reply('#Q_LikeTheActivity', { activity_name: "l'Orient Express" });
   });
 
   // -----------------------------------------------------------------------
@@ -43,11 +57,36 @@ module.exports = function(bp) {
   bp.hear('QR_NO_LAST_MINUTE_DIFFICULTIES', (event, next) => {
     event.reply('#R-_lastMinuteDifficulties');
   });
-  bp.hear('QR_KEEP_CALM_1' | 'QR_KEEP_CALM_2', (event, next) => {
+
+  bp.hear('QR_KEEP_CALM_1', (event, next) => {
+    bp.db.get()
+    .then(knex => 
+      knex('skills')
+      .insert({ user_id: '1638202549569353', stressControl: 1}));
     event.reply('#R+_12_lastMinuteDifficulties');
   });
-  bp.hear('QR_KEEP_CALM_3' | 'QR_KEEP_CALM_4', (event, next) => {
+  bp.hear('QR_KEEP_CALM_2', (event, next) => {
+    bp.db.get()
+    .then(knex => knex('skills')
+      .where({ user_id: '1638202549569353' })
+        .update({stressControl: 2}));
+    event.reply('#R+_12_lastMinuteDifficulties');
+  });
+  bp.hear('QR_KEEP_CALM_3', (event, next) => {
+    bp.db.get()
+    .then(knex => knex('skills')
+      .where({ userId: '1638202549569353' })
+        .update({stressControl: 3}));
     event.reply('#R+_34_lastMinuteDifficulties');
+  });
+  bp.hear('QR_KEEP_CALM_4', (event, next) => {
+    bp.db.get()
+    .then(knex => knex('skills')
+      .where({ userId: '1638202549569353' })
+        .update({stressControl: 4}));
+    event.reply('#R+_34_lastMinuteDifficulties');
+
+
   });
   bp.hear('QR_GROUP_GOAL_NOT_ENOUGH', (event, next) => {
     event.reply('#R-_GroupGoal');
@@ -57,16 +96,25 @@ module.exports = function(bp) {
   });
 
   // Group goal response and respecting deadline question
-  bp.hear(' QR_RESPECTING_DEADLINES_YES', (event, next) => {
+  bp.hear('QR_RESPECTING_DEADLINES_YES', (event, next) => {
     event.reply('#R+_Deadlines');
   });
-  bp.hear(' QR_RESPECTING_DEADLINES_NO', (event, next) => {
+  bp.hear('QR_RESPECTING_DEADLINES_NO', (event, next) => {
     event.reply('#R-_Deadlines');
   });
 
   // Respecting deadlines response and question about feedback to the responsible of communication
-  bp.hear('QR_FEEDBACK_RESPO_COMMUNICATION_1' | 'QR_FEEDBACK_RESPO_COMMUNICATION_2' | 'QR_FEEDBACK_RESPO_COMMUNICATION_3' | 'QR_FEEDBACK_RESPO_COMMUNICATION_4', (event, next) => {
+  bp.hear('QR_FEEDBACK_RESPO_COMMUNICATION_1', (event, next) => {
     event.reply('#Graph_Goodbye');
+  });
+  bp.hear('QR_FEEDBACK_RESPO_COMMUNICATION_2', (event, next) => {
+    event.reply('#R-_Deadlines');
+  });
+  bp.hear('QR_FEEDBACK_RESPO_COMMUNICATION_3', (event, next) => {
+    event.reply('#R-_Deadlines');
+  });
+  bp.hear('QR_FEEDBACK_RESPO_COMMUNICATION_4', (event, next) => {
+    event.reply('#R-_Deadlines');
   });
 
 
